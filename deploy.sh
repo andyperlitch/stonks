2267114f04c148d49e7bfc429dc7766b2ed22a9d
Andy Perlitch <aperlitch@netflix.com>
2021-03-08 16:37:11 -0800

    docker deploy, attempt 1

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# go to the project directory
+cd ~/stonks
+
+# pull latest code
+git pull origin master
+
+# build the docker image for the app
+docker-compose -f ./docker/prod/docker-compose.yml up -d
--- start-docker-dev.bash
+++ start-docker-dev.bash
@@ -13,6 +13,10 @@ docker-sync start -c $DOCKER_SYNC_FILE
 echo "====> Running docker-compose"
 docker-compose -f ./docker/dev/docker-compose.yml up -d
 # enter into the dev container
+
+echo "====> Copying env file"
+docker cp .env stonks_app:/root/app/
+
 echo "====> Entering app dev container"
 # name of the container must match container_name in docker-compose file
 docker exec -it stonks_app bash --init-file docker/dev/.profile
